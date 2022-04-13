6984d1a5c77cc5a72be7f87e7f4e5f2030d54917
grey4uk <grey4uk14@gmail.com>
2022-04-13 21:04:29 +0300

    changed package.json

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { App } from 'components/App';
 import './index.css';
 
-const container = document.getElementById('root');
-
-ReactDOM.createRoot(container).render(
- 
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-
+  </React.StrictMode>,
+  document.getElementById('root')
 );
