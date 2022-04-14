d91833a1732c00eddf145003d08b22b27b487841
grey4uk <grey4uk14@gmail.com>
2022-04-14 12:46:48 +0300

    new branch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,59 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Button from './Button/Button';
+
+export class App extends Component {
+  // constructor(props){
+  //   super(props);
+  //   this.state={value:props}
+  // }
+
+  state = { value: 'placeholder', hasError: '' };
+
+  componentWillUnmount() {
+    alert('unmount!!!');
+  }
+
+  componentDidCatch(error, info) {
+    console.log('error :>> ', error);
+    console.log('info :>> ', info);
+    // Если метод был вызван значит есть ошибка!
+    // Устанавливаем состояние
+    const errArray = info.componentStack.split(' ');
+    const componentIdx = errArray.findIndex(
+      (el) => el === 'at'
+    );
+
+    this.setState({
+      hasError: `Trouble in component>>> ${
+        errArray[componentIdx + 1]
+      } <<<<`,
+    });
+    // Также можно отправить отчет об ошибке вашему аналитическому сервису
+    // logErrorToMyService(error, info);
+  }
+
+  render() {
+    const { value, hasError } = this.state;
+    return (
+      <>
+        {hasError ? (
+          <>
+            <p>Somthing wrong...</p>
+            <p>{hasError}</p>
+          </>
+        ) : (
+          <>
+            <h1>HELLO!!!</h1>
+            <Button title={value} />
+            <button
+              onClick={() =>
+                this.setState({ value: 'show alert' })
+              }>
+              APP: SET TITLE
+            </button>
+          </>
+        )}
+      </>
+    );
+  }
+}
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,67 @@
+import { Component } from 'react';
+
+class Button extends Component {
+  state = { title: '', array: [1, 2, 3] };
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    //     console.log('getDerivedStateFromProps>>>');
+    return {
+      ...prevState,
+      title: prevState.title || nextProps.title,
+    };
+  }
+
+  //   componentDidMount() {
+  //     console.log('componentDidMount>>>>>>');
+  //     this.setState({ title: this.props.title });
+  //   }
+
+  sum = () =>
+    this.state.array.reduce((acc, el) => acc + el);
+
+  shouldComponentUpdate(nextProps, nextState) {
+    //     console.log('shouldComponentUpdate>>>');
+
+    //     console.log('nextProps>>>', nextProps);
+    //     console.log('nextState', nextState);
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('getSnapshotBeforeUpdate______');
+    console.log('prevProps :>> ', prevProps);
+    console.log('prevState :>> ', prevState);
+    return this.sum();
+  }
+
+  componentDidUpdate(prevProps, prevState, shout) {
+    //     console.log('shout :>> ', shout);
+
+    console.log('componentDidUpdate++++++++');
+
+    if (this.props.title !== prevProps.title) {
+      this.setState({ title: this.props.title });
+      //       console.log('PROPS :>> ', this.props.title);
+      //       console.log('PROPS :>> ', prevProps.title);
+    }
+
+    if (prevState.title !== this.state.title) {
+      this.setState({ title: this.state.title });
+      //       console.log('STATE :>> ', this.state.title);
+      //       console.log('STATE :>> ', prevState.title);
+    }
+  }
+
+  render() {
+    const { title, pet } = this.state;
+    return (
+      <button
+        type='button'
+        onClick={() => this.setState({ title: 'Show' })}>
+        Button: {title}({pet.length})
+      </button>
+    );
+  }
+}
+
+export default Button;
