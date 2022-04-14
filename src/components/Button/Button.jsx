aa8242424096e3aa10f94dd3df0efda67e3a396e
grey4uk <grey4uk14@gmail.com>
2022-04-14 13:15:21 +0300

    addedNeededConsolesToComponentsOfLifycycle

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,23 +4,23 @@ class Button extends Component {
   state = { title: '', array: [1, 2, 3] };
 
   static getDerivedStateFromProps(nextProps, prevState) {
-    //     console.log('getDerivedStateFromProps>>>');
+    console.log('getDerivedStateFromProps>>>');
     return {
       ...prevState,
       title: prevState.title || nextProps.title,
     };
   }
 
-  //   componentDidMount() {
-  //     console.log('componentDidMount>>>>>>');
-  //     this.setState({ title: this.props.title });
-  //   }
+  componentDidMount() {
+    console.log('componentDidMount>>>>>>');
+    //       this.setState({ title: this.props.title });
+  }
 
   sum = () =>
     this.state.array.reduce((acc, el) => acc + el);
 
   shouldComponentUpdate(nextProps, nextState) {
-    //     console.log('shouldComponentUpdate>>>');
+    console.log('shouldComponentUpdate>>>');
 
     //     console.log('nextProps>>>', nextProps);
     //     console.log('nextState', nextState);
@@ -34,8 +34,8 @@ class Button extends Component {
     return this.sum();
   }
 
-  componentDidUpdate(prevProps, prevState, shout) {
-    //     console.log('shout :>> ', shout);
+  componentDidUpdate(prevProps, prevState, shot) {
+    //     console.log('shot :>> ', shot);
 
     console.log('componentDidUpdate++++++++');
 
